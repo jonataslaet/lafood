10b424ac5898c475399784a2d8dacf60c10402f0
jonatas.laet <jonataslaetprogramador@gmail.com>
2022-05-18 16:51:37 -0300

    main
    Criação do projeto

---
+++ pedido/src/test/java/com/github/jonataslaet/lafood/pedido/HelloResourceIT.java
@@ -0,0 +1,9 @@
+package com.github.jonataslaet.lafood.pedido;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class HelloResourceIT extends HelloResourceTest {
+
+    // Execute the same tests but in native mode.
+}
