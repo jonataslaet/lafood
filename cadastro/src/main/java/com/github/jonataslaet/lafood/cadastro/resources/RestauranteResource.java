bffb12c458100a49e1a8a87fe1da3d8d24b722df
jonataslaet <jonataslaetprogramador@gmail.com>
2022-07-13 23:36:38 -0300

    microsservico_cadastro
    Colocando response no retorno das respostas
    Adicionados os endpoints de atualizar e deletar restaurante

--- cadastro/src/main/java/com/github/jonataslaet/lafood/cadastro/resources/RestauranteResource.java
+++ cadastro/src/main/java/com/github/jonataslaet/lafood/cadastro/resources/RestauranteResource.java
@@ -1,14 +1,21 @@
 package com.github.jonataslaet.lafood.cadastro.resources;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import com.github.jonataslaet.lafood.cadastro.entities.Restaurante;
 
@@ -24,7 +31,35 @@ public class RestauranteResource {
     
     @POST
     @Transactional
-    public void adicionar(Restaurante entity) {
+    public Response adicionar(Restaurante entity) {
     	entity.persist();
+    	return Response.status(Status.CREATED).build();
+    }
+    
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public Response atualizar(@PathParam("id") Long id, Restaurante entity) {
+    	Optional<Restaurante> findById = Restaurante.findByIdOptional(id);
+    	if (findById.isEmpty()) {
+    		throw new NotFoundException();
+    	}
+    	Restaurante restaurante = findById.get();
+    	restaurante.proprietario = entity.proprietario;
+    	restaurante.persist();
+    	return Response.status(Status.NO_CONTENT).build();
+    }
+    
+    @DELETE
+    @Path("{id}")
+    @Transactional
+    public Response deletar(@PathParam("id") Long id) {
+    	Optional<Restaurante> findById = Restaurante.findByIdOptional(id);
+    	if (findById.isEmpty()) {
+    		throw new NotFoundException();
+    	}
+    	Restaurante restaurante = findById.get();
+    	restaurante.delete();
+    	return Response.status(Status.NO_CONTENT).build();
     }
 }
